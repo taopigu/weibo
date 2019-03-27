0b6b1dcd4221f5935d55c7f54ef3f84f0ee59b21
Wudi <973322300@qq.com>
2019-03-27 14:32:21 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
     <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1" alt="">
     <a href="https://laravel-china.org/courses" target="_blank" title="">刻意练习，每日精进</a>
     <div class="float-right">
-        <a href="/about" title="">关于</a>
+        <a href="{{ route('about') }}" title="">关于</a>
     </div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/" title="">Weibo App</a>
+            <a class="navbar-brand" href="{{ route('home') }}" title="">Weibo App</a>
             <ul class="navbar-nav justify-content-end">
-                <li class="nav-item"><a class="nav-link" href="/help" title="">帮助</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('help') }}" title="">帮助</a></li>
                 <li class="nav-item"><a class="nav-link" href="#" title="">登陆</a></li>
             </ul>
         </div>
