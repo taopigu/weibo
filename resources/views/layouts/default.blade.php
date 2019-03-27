7d2de7f484fb861d823ea5b06773f317323d1c87
Wudi <973322300@qq.com>
2019-03-27 14:27:50 +0800

    切割头部和尾部子视频

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,18 +6,11 @@
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/" title="">Weibo App</a>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item"><a class="nav-link" href="/help" title="">帮助</a></li>
-                <li class="nav-item"><a class="nav-link" href="#" title="">登陆</a></li>
-            </ul>
-        </div>
-    </nav>
-
+    @include("layouts._header")
     <div class="container">
         @yield('content')
+
+        @include("layouts._footer")
     </div>
 </body>
 </html>
