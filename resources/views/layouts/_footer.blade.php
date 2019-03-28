f18cc33a41024ec918d87493d57f8f1f625a34fe
Wudi <973322300@qq.com>
2019-03-28 11:13:46 +0800

    登录成功后导航逻辑

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,4 @@
 <footer class="footer">
-    <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1" alt="">
     <a href="https://laravel-china.org/courses" target="_blank" title="">刻意练习，每日精进</a>
     <div class="float-right">
         <a href="{{ route('about') }}" title="">关于</a>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -16,9 +16,12 @@
 
             @include("layouts._footer")
         </div>
-
-
     </div>
+
+
+
+
+    <script type="text/javascript" src="{{ mix('js/app.js')}}"></script>
 </body>
 </html>
 
