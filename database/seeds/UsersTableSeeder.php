d78c04cb74f72b542a4857c1cbcfd441c994c9e2
Wudi <973322300@qq.com>
2019-03-28 17:45:59 +0800

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,7 @@ class UsersTableSeeder extends Seeder{
         $user = User::find(1);
         $user->name = 'wd';
         $user->email = 'wd@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 
